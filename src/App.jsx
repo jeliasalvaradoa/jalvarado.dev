c772c49cbd7baa5a3a9232c26c61e5eb804c4d99
jjaa <josealvarado30@hotmail.com>
2023-11-25 22:10:44 -0400

    actualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //import './App.css'
-import './assets/css/styles.css'
+//import './assets/css/styles.css'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { UnderFooter } from './components/UnderFoorter'
