62ac66868f1bd8078db71596762f00d5852ac259
jjaa <josealvarado30@hotmail.com>
2023-11-26 10:51:01 -0400

    actualizando

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -76,3 +76,72 @@ export function Menu() {
     </nav>
   );
 }
+
+
+
+
+
+
+
+import { useState, useEffect, useRef } from 'react';
+
+export function Menu() {
+  const [isSticky, setIsSticky] = useState(false);
+  const myNavRef = useRef(null);
+  const parallaxElementsRef = useRef([]);
+
+  useEffect(() => {
+    const myNav = myNavRef.current;
+    const parallaxElements = parallaxElementsRef.current;
+
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const stickyNavigationOffsetTop = myNav.offsetTop;
+
+      if (scrollY > stickyNavigationOffsetTop) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+
+      parallaxElements.forEach((element) => {
+        const speed = element.getAttribute('parallax-speed');
+        if (speed) {
+          const backgroundPos = `center -${scrollY / speed}px`;
+          element.style.backgroundPosition = backgroundPos;
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav ref={myNavRef} className={`navbar navbar-default navbar-sticky${isSticky ? ' stick' : ''}`}>
+      <div className="container-fluid">
+        
+
+      
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="index.html">Inicio</a>
+            </li>
+            <li>
+              <a href="about.html">Acerca de</a>
+            </li>
+            <li>
+              <a href="single.html">Contacto</a>
+            </li>
+            <li>
+              <a href="blog.html">Blog</a>
+            </li>
+          </ul>
+        </div>
+      
+    </nav>
+  );
+}
